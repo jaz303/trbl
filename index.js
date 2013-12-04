562b3f8ccff99754c4d8fe90fd6681b2e44de085
Jason Frame <jason@onehackoranother.com>
2013-12-04 10:07:19 +0000

    remove ref to hk.parseInt

--- index.js
+++ index.js
@@ -8,37 +8,37 @@ module.exports = function(thing) {
         switch (thing.length) {
             case 1:
                 return [
-                    hk.parseInt(thing[0], 10),
-                    hk.parseInt(thing[0], 10),
-                    hk.parseInt(thing[0], 10),
-                    hk.parseInt(thing[0], 10)
+                    parseInt(thing[0], 10),
+                    parseInt(thing[0], 10),
+                    parseInt(thing[0], 10),
+                    parseInt(thing[0], 10)
                 ];
             case 2:
                 return [
-                    hk.parseInt(thing[0], 10),
-                    hk.parseInt(thing[1], 10),
-                    hk.parseInt(thing[0], 10),
-                    hk.parseInt(thing[1], 10)
+                    parseInt(thing[0], 10),
+                    parseInt(thing[1], 10),
+                    parseInt(thing[0], 10),
+                    parseInt(thing[1], 10)
                 ];
             case 3:
                 return [
-                    hk.parseInt(thing[0], 10),
-                    hk.parseInt(thing[1], 10),
-                    hk.parseInt(thing[2], 10),
-                    hk.parseInt(thing[1], 10)
+                    parseInt(thing[0], 10),
+                    parseInt(thing[1], 10),
+                    parseInt(thing[2], 10),
+                    parseInt(thing[1], 10)
                 ];
             case 4:
                 return [
-                    hk.parseInt(thing[0], 10),
-                    hk.parseInt(thing[1], 10),
-                    hk.parseInt(thing[2], 10),
-                    hk.parseInt(thing[3], 10)
+                    parseInt(thing[0], 10),
+                    parseInt(thing[1], 10),
+                    parseInt(thing[2], 10),
+                    parseInt(thing[3], 10)
                 ];
             default:
                 throw new Error("trbl - array must have 1-4 elements");
         }
     } else {
-        var val = hk.parseInt(thing);
+        var val = parseInt(thing);
         return [val, val, val, val];
     }
 }
